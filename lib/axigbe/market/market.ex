b70b66735f942c9743350de3cde6e299c1aa3432
Kpêdido Godonou <rgodonou@africanschoolofeconomics.com>
2024-09-04 04:05:54 +0100

    Deploy Prototype Branch

---
+++ lib/axigbe/market/market.ex
@@ -0,0 +1,13 @@
+# defmodule Axigbe.Market do
+#   use Ash.Domain
+
+#   resources do
+#     resource Axigbe.Market.Business do
+#       define :create_business, action: :create
+#       define :list_businesses, action: :read
+#       define :update_business, action: :update
+#       define :delete_business, action: :destroy
+#       define :get_business, args: [:id], action: :by_id
+#     end
+#   end
+# end
